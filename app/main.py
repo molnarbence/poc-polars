29352781efd2d194fd24fcc53b5d74d6cb6f3647
Bence Molnar <xjarekx@gmail.com>
2024-06-24 22:47:13 +0200

    Setup python CI

--- app/main.py
+++ app/main.py
@@ -1,19 +1,20 @@
-import psycopg
-
 import polars as pl
-
+import psycopg
 
 
 def foo(a: int, b: int) -> int:
     return a + b
 
+
 def query_local_db() -> None:
-    with psycopg.connect("host=localhost port=5432 dbname=postgres user=postgres password=postgres connect_timeout=10") as conn:
+    with psycopg.connect(
+        "host=localhost port=5432 dbname=postgres user=postgres password=postgres connect_timeout=10"
+    ) as conn:
         foo_df = pl.read_database("SELECT * from foo.bar", connection=conn)
 
         foo_df.write_csv("./outputs/bar.csv")
         foo_df.write_parquet("./outputs/bar.parquet")
-        
+
 
 if __name__ == "__main__":
     query_local_db()
